089667b1da7e80aa1931643f9926b736148961c9
evansch1976 <evansch@gmail.com>
2025-01-21 10:06:48 -0500

    Adding region variable

--- providers.tf
+++ providers.tf
@@ -7,5 +7,6 @@ terraform {
 }
 
 provider "aws" {
-  region  = "us-west-2"
+  #region  = "us-west-2"
+  region  = var.aws_region
 }
--- variables.tf
+++ variables.tf
@@ -2,3 +2,7 @@ variable "instance_type" {
   description = "Type of EC2 instance to provision"
   default     = "t3.nano"
 }
+variable "aws_region" {
+  description = "Region to deploy to"
+  default     = "ca-central-1"
+}
